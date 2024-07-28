74df338ffbe833e6db4855a8e417d5888b07fe28
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-07-28 00:06:15 -0500

    Primer commit

---
+++ src/proyectovocesnoche/ProyectoVocesNoche.java
@@ -0,0 +1,13 @@
+package proyectovocesnoche;
+
+/**
+ *
+ * @author daceb
+ */
+public class ProyectoVocesNoche {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
